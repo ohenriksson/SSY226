e171bb7b0309ef14ddc269122411c0a31ecff02e
Oskar Henriksson <o.henriksson@gmail.com>
2017-10-30 16:42:21 +0100

    A simple script to read and plot the layout
    Points are plotted but not edges

---
+++ PointPlotter.py
@@ -0,0 +1,59 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class PointPlotter:
+
+    points = []
+    segments = []
+    def __init__(self ,filename):
+        self.filename = filename
+        self.main()
+
+    def main(self):
+        f = open(self.filename, "r")
+
+        while f.readable():
+            line = f.readline()
+            cont_read = self.read_row(line)
+            if not cont_read:
+                break
+
+        f.close()
+        return
+
+    def read_row(self, string_line):
+        string_line = string_line.rstrip("\n")
+        row = string_line.rsplit(" ")
+
+        if row[0] == 'point':
+            self.read_point(row)
+            return True
+        elif row[0] == 'segment':
+            self.read_segment(row)
+            return True
+        else:
+            return False
+
+    def read_point(self ,arrayPoint):
+        point = [arrayPoint[2] ,arrayPoint[3]]
+        self.points.append(point)
+
+
+    def read_segment(self ,arraySegment):  # not complete
+        segment = [arraySegment[2] ,arraySegment[3]]
+        self.segments.append(segment)
+
+    def plot(self):
+        fig, ax = plt.subplots()
+        x = [x[0] for x in self.points]
+        y = [x[1] for x in self.points]
+        ax.scatter(x, y)
+        ax.grid()
+        plt.show()
+        print(self.points)
+
+
+
+pp = PointPlotter("layout.txt")
+pp.plot()
