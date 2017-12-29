b60b70e707fb3f7c4d90b32df60a2f4759217ada
Oskar Henriksson <o.henriksson@gmail.com>
2017-12-29 12:26:50 +0100

    Correct tasks_must_go_on

--- PySolver/Model2.py
+++ PySolver/Model2.py
@@ -11,7 +11,8 @@ class Model2:
     taskLowerBound = 1
     nodeCap = 1
     edgeCap = 1
-    T = 10
+    T = 18
+    agvMax = 2
 
     snk_tasks = [1,1,3]
     src_tasks = [0,1,2]
@@ -29,6 +30,7 @@ class Model2:
 
     Y = LpVariable.dicts('Y', (NODES, TASK, TIME), lowBound=0, cat=LpInteger)
     X = LpVariable.dicts('X', (NODES, NODES, TASK, TIME), lowBound=0, upBound=edgeCap, cat=LpInteger)
+    AGVS = LpVariable('AGVS',lowBound=0, upBound=agvMax, cat=LpInteger)
 
     @classmethod
     def init(cls):
@@ -41,13 +43,15 @@ class Model2:
         cls.tasks_must_be_dropped(prob)
         cls.node_capacity(prob)
         cls.tasks_lower_bound(prob)
-        # cls.start_node_task(prob) #not working atm
+        cls.start_node_task(prob) #does not seem to restrict further
+        cls.agv_number_restrictions(prob)
 
         #prob.writeLP("MinmaxProblem.lp")
         prob.solve()
 
         for v in prob.variables():
-            print(v.name, "=", v.varValue)
+            if v.varValue > 0:
+                print(v.name, "=", v.varValue)
         print( "Throughput :", pulp.value(prob.objective))
 
     @classmethod
@@ -65,7 +69,7 @@ class Model2:
         for k in cls.TIME:
             for t in cls.TASK:
                 for v0 in cls.NODES:
-                   arcs = cls.arcs_ending_here(v0, k)
+                   arcs = cls.arcs_ending_at(v0, k)
                    label = 'detector_' +str(k) + '_' +str(t) +'_' +str(v0)
                    incoming = [cls.X[a[cls.a_src]][v0][t][k-a[cls.a_dst]] for a in arcs]
                    prob += lpSum(incoming) == cls.Y[v0][t][k], label
@@ -75,7 +79,7 @@ class Model2:
     def inflow_outflow(cls, prob):
         for k in cls.TIME:
             for v0 in cls.INTER:
-                arcsIn = cls.arcs_ending_here(v0,k)
+                arcsIn = cls.arcs_ending_at(v0, k)
                 arcsOut = cls.arcs_starting_here(v0,k)
                 inArcs = [[ cls.X[a[cls.a_src]][a[cls.a_snk]][t][k-a[cls.a_dst]] for t in cls.TASK] for a in arcsIn]
                 outArcs =[[ cls.X[a[cls.a_src]][a[cls.a_snk]][t][k] for t in cls.TASK] for a in arcsOut]
@@ -95,16 +99,17 @@ class Model2:
         for k in cls.TIME:
             for t in cls.TASKLIST:
                 for v0 in cls.INTER:
-                    arcsOut = cls.arcs_starting_here(v0,k)
-                    constraint = cls.Y[v0][t][k] == lpSum([ cls.X[a[cls.a_src]][a[cls.a_snk]][t][k] for a in arcsOut])
-                    prob += constraint, ""
+                    if v0 != cls.src_tasks[t] and v0 != cls.snk_tasks[t]:
+                        arcsOut = cls.arcs_starting_here(v0,k)
+                        constraint = cls.Y[v0][t][k] == lpSum([ cls.X[a[cls.a_src]][a[cls.a_snk]][t][k] for a in arcsOut])
+                        prob += constraint, ""
 
     @classmethod
     def tasks_must_be_dropped(cls, prob):
         for k in cls.TIME:
             for t in cls.TASKLIST:
-                for v0 in list(filter(lambda v: v == cls.snk_tasks[t],cls.INTER)):
-                    arcsIn = cls.arcs_ending_here(v0,k)
+                for v0 in list(filter(lambda v: v == cls.snk_tasks[t], cls.INTER)):
+                    arcsIn = cls.arcs_ending_at(v0, k)
                     prob += lpSum([cls.X[a[cls.a_src]][v0][t][k] for a in arcsIn]) == 0
 
     @classmethod
@@ -128,11 +133,28 @@ class Model2:
                     prob += cls.X[cls.startNode][arcDest][t][k] == 0, ""
 
     @classmethod
-    def arcs_ending_here(cls, v0, k)->[]:
-        arcs = list(filter(lambda s: s[cls.a_snk] == v0 and k-s[cls.a_dst] >= 0, cls.ARCS))
-        return arcs
+    def agv_number_restrictions(cls, prob):
+        cls.agv_counter(prob)
+
+    @classmethod
+    def agv_counter(cls,prob):
+         startArcs = cls.arcs_starting_here(cls.startNode)
+         endArcs = cls.arcs_ending_here(cls.endNode)
+         startSum = list(cls.X[a[cls.a_src]][a[cls.a_snk]] for a in startArcs)
+         endSum = [cls.X[a[cls.a_src]][a[cls.a_snk]] for a in endArcs]
+         prob += lpSum(startSum) == lpSum(endSum), "agv restrictor"
+         prob += lpSum(startSum) == cls.AGVS, "counter"
+
+
+    @classmethod
+    def arcs_ending_at(cls, v0, k)->[]:
+        arcs = cls.arcs_ending_here(v0)
+        return list(filter(lambda s: k-s[cls.a_dst] >= 0, arcs))
+
+    @classmethod
+    def arcs_ending_here(cls,v0)->[]:
+        return list(filter(lambda s: s[cls.a_snk] == v0, cls.ARCS))
 
     @classmethod
     def arcs_starting_here(cls, v0, k=0)->[]:
-        arcs = list(filter(lambda s: s[cls.a_src] == v0 , cls.ARCS))
-        return arcs
+        return list(filter(lambda s: s[cls.a_src] == v0 , cls.ARCS))
